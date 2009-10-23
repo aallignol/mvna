ff69db467c1de5a922b2549e9515fb6eb4846af3
Arthur Allignol <arthur.allignol@fdm.uni-freiburg.de>
2009-10-23 18:34:02 +0200

    removed the old code that was commented

--- R/predict.mvna.R
+++ R/predict.mvna.R
@@ -29,31 +29,3 @@ predict.mvna <- function(object, times, tr.choice, level = 0.95,
 
     res
 }
- 
-
-
-## "predict.mvna" <-
-## function(object,times,tr.choice,...) {
-##   if(!inherits(object,"mvna")) {
-##     stop("Argument 'object' must be of class \"mvna\".")
-##   }
-##   if (sum(times < 0) >=1)
-##     stop("Negative 'times' may be problematic")
-##   if (missing(tr.choice)) {
-##     tr.choice <- names(object[1:(length(object)-7)])
-##   }
-##   object <- object[tr.choice]
-##   if (sum(tr.choice %in% names(object)) != length(tr.choice)) {
-##     stop("Transition names given in 'tr.choice' must match with 'object'")
-##   }
-##   res <- lapply(1:length(object), function(i) {
-##     ind <- findInterval(times, object[[i]]$time)
-##     ind[ind==0] <- NA
-##     temp <- object[[i]][ind, ]
-##     temp[is.na(temp$time), ] <- 0
-##     temp$time <- times
-##     temp
-##   })
-##   res
-## }
-    
