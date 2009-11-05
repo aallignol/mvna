faf60b3b69636a395f5c4b8a9261751784e85aa8
Arthur Allignol <arthur.allignol@fdm.uni-freiburg.de>
2009-11-05 11:21:00 +0100

    mvna2tdc almost works chose finally to use logical vectors to get what I need
    TO DO:
    * Still a problem with conversion from factor for status
    * Need to test when there's several time-dependent covariates

--- R/transfoData.R
+++ R/transfoData.R
@@ -1,3 +1,57 @@
 mvna2tdc <- function(data, state.names, tra, cens.name, cov.states, ...) {
     
+    colnames(tra) <- rownames(tra) <- state.names
+    t.from <- lapply(1:dim(tra)[2], function(i) {
+        rep(rownames(tra)[i], sum(tra[i, ]))
+    })
+    t.from <- unlist(t.from)
+    t.to <- lapply(1:dim(tra)[2], function(i) {
+        colnames(tra)[tra[i, ]==TRUE]
+    })
+    t.to <- unlist(t.to)
+    trans <- data.frame(from=t.from, to=t.to)
+    namen <- paste(trans[, 1], trans[, 2])
+
+    absorb <- setdiff(levels(trans$to), levels(trans$from))
+    transient <- unique(state.names[!(state.names %in% absorb)])
+
+### If not, put counting process like
+    if ("time" %in% names(data)) {
+        data <- data[order(data$id, data$time), ]
+        idd <- as.integer(data$id)
+        entree <- double(length(data$time))
+        masque <- rbind(1, apply(as.matrix(idd), 2, diff))
+        entree <- c(0, data$time[1:(length(data$time) - 1)]) * (masque == 0)
+        data <- data.frame(id = data$id, from = data$from,
+                           to = data$to, entry = entree, exit = data$time)
+        if (sum(data$entry < data$exit) != nrow(data))
+            stop("Exit time from a state must be > entry time")
+    } else {
+        if (sum(data$entry < data$exit) != nrow(data))
+            stop("Exit time from a state must be > entry time")
+    }
     
+### Get rid of these annoying factors, keeping the original
+### state.numbers (won't be really possible, especially if
+### the state.names are words)
+### Found another way: create indices with what's interesting, and then get rid of the factors
+    ind.cens <- data$to == cens.name
+    ind.absorb <- data$to %in% absorb
+    ## This part will be a bit more tricky
+    ind.c <- data$from %in% cov.states
+    ind.cov <- matrix(FALSE, nrow = nrow(data), ncol = length(cov.states))
+    ind.cov[ind.c, match(data$from[ind.c], cov.states)] <- TRUE
+
+### Let's create the data set now
+    id <- data$id
+    status <- as.numeric(as.character(data$to)) * ind.absorb
+    colnames(ind.cov) <- paste("cov", cov.states, sep = ".")
+    ind.cov <- as.numeric(ind.cov)
+    new.data <- data.frame(id = id,
+                           entry = data$entry,
+                           exit = data$exit,
+                           ind.cov,
+                           status = status)
+
+    new.data
+}
